5682cea851dfca020ce68ec1771cce4a8ec7adfc
bjlara1 <bjlara1@cps.edu>
2024-09-18 16:40:04 -0500

    Hardware ID fixing
    Fixed hardware Id's, allowing the robot to move correctly forward, backwards, and strafing.

--- fuck robots dude/Jimbo/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ fuck robots dude/Jimbo/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -124,8 +124,10 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE); // add if needed
-        rightRear.setDirection(DcMotorSimple.Direction.REVERSE); // add if needed
+     //  rightFront.setDirection(DcMotorSimple.Direction.REVERSE); // add if needed
+       // leftRear.setDirection(DcMotorSimple.Direction.REVERSE); // add if needed
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
         List<Integer> lastTrackingEncPositions = new ArrayList<>();
         List<Integer> lastTrackingEncVels = new ArrayList<>();
