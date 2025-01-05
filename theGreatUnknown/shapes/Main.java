16392bd55964014b9d45979d704d5b664d0b71e7
ian <irbrost@cps.edu>
2025-01-05 12:29:06 -0600

    IMADETHEWATERMELONMOVEHOLYSHIT

--- theGreatUnknown/shapes/Main.java
+++ theGreatUnknown/shapes/Main.java
@@ -1,9 +1,11 @@
+
 class Main
 {
     static Game game;
     public static void main(String[] args)
     {
         game = new Game();
+
         while(true)
         {
             game.update();
--- theGreatUnknown/shapes/Screen.java
+++ theGreatUnknown/shapes/Screen.java
@@ -1,19 +1,48 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Screen {
+public class Screen extends JPanel{
 
-    JFrame screen;
+    int windowX;
+    int windowY;
+    int mouseX;
+    int mouseY;
+    BufferedImage watermelon;
 
     public Screen()
     {
-        screen = new JFrame("game");
-        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        screen.setSize(500, 600);
-        screen.setVisible(true);
+        setPreferredSize(new Dimension(800, 800));
+        setBackground(new Color(5, 10, 40));   
+        try
+        {
+            watermelon = ImageIO.read(new File("C:/Users/irbrost/Documents/GitHub/industrialFailure/theGreatUnknown/shapes/Images/watermelon.png"));
+        }
+        catch(IOException error)
+        {
+            System.out.println("oh no");
+        }
     }
 
-    public void update()
+    public void giveWindow(int x, int y)
     {
-        screen.setBackground(new Color(0, 0, 0));
+        windowX = x;
+        windowY = y;
     }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        mouseX = MouseInfo.getPointerInfo().getLocation().x;
+        mouseX = mouseX - windowX;
+        mouseY = MouseInfo.getPointerInfo().getLocation().y;
+        mouseY = mouseY - windowY;
+
+        super.paintComponent(g);
+        g.drawImage(watermelon,mouseX - watermelon.getWidth()/2, mouseY - watermelon.getHeight()/2, this);      
+    }
+
 }
